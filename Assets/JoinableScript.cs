60aeccd1c1275d81804cf7dc32006443fd80da7f
knwrpw <knwr@mini.pw.edu.pl>
2017-12-16 19:10:49 +0100

    Hackathon 2

---
+++ Assets/JoinableScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinableScript : MonoBehaviour {
+
+    public GameObject tree;
+    private bool isInTree = false;
+
+    // Use this for initialization
+    void Start () {
+        tree = GameObject.Find("Fir_Tree");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isInTree && !(GetComponent<OVRGrabbable>()).isGrabbed)
+        {
+            transform.parent = tree.transform;
+            transform.rotation = Quaternion.identity;
+            GetComponent<Rigidbody>().freezeRotation = true;
+            //Destroy(GetComponent<OVRGrabbable>());
+            //Destroy(GetComponent<SphereCollider>());
+            //Destroy(GetComponent<Rigidbody>());
+            //Destroy(this);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject == tree)
+        {
+            isInTree = true;
+
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+
+        if (collision.gameObject == tree)
+        {
+            isInTree = false;
+        }
+    }
+
+}
---
+++ Assets/TreeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    List<GameObject> ToFreeze = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //collision.gameObject.transform.parent = gameObject.transform;
+        //Destroy(GetComponent<OVRGrabbable>());
+        Destroy(GetComponent<SphereCollider>());
+        Destroy(GetComponent<Rigidbody>());
+        Destroy(this);
+    }
+}
